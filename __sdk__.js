ad33e828897fbcfe152fc99d7f2090f13f9fb3d0
Daniel Brain <dbrain@paypal.com>
2018-04-17 16:47:50 -0700

    Use new format for __sdk__.js

--- __sdk__.js
+++ __sdk__.js
@@ -3,54 +3,53 @@
 
 module.exports = {
 
-    // Set up `?modules=lebowski-pay` to include `./src/index`
-    modules: {
-        'lebowski-pay': './src/index'
-    },
+    'lebowski-pay': {
+        entry: './src/index',
 
-    // Set up a namespace for any module-specific build-time flags
-    staticNamespace: 'LEBOWSKIPAY',
+        // Set up a namespace for any module-specific build-time flags
+        staticNamespace: 'LEBOWSKIPAY',
 
-    // Set up a graphql config query
-    configQuery: `
-        clientConfiguration {
-            assetsUrl
-        }
-    `,
+        // Set up a graphql config query
+        configQuery: `
+            clientConfiguration {
+                assetsUrl
+            }
+        `,
 
-    // Configure features specific to this module
-    features: {
+        // Configure features specific to this module
+        features: {
 
-        date: {
-            // Deprecate feature X from 2017/06/23 onwards
-            '2017-06-23': {
-                FEATURE_X: false
+            date: {
+                // Deprecate feature X from 2017/06/23 onwards
+                '2017-06-23': {
+                    FEATURE_X: false
+                },
+
+                // Enable feature Y from 2018/02/09 onwards
+                '2018-02-09': {
+                    FEATURE_Y: true
+                }
             },
 
-            // Enable feature Y from 2018/02/09 onwards
-            '2018-02-09': {
-                FEATURE_Y: true
-            }
-        },
+            country: {
+                // Enable feature Z for FR
+                FR: {
+                    FEATURE_Z: true
+                }
+            },
 
-        country: {
-            // Enable feature Z for FR
-            FR: {
-                FEATURE_Z: true
-            }
-        },
+            partner: {
+                // Enable feature A for partner XYZ
+                XYZ: {
+                    FEATURE_A: true
+                }
+            },
 
-        partner: {
-            // Enable feature A for partner XYZ
-            XYZ: {
-                FEATURE_A: true
-            }
-        },
-
-        merchant: {
-            // Enable feature B for merchant ABC
-            ABC: {
-                FEATURE_B: true
+            merchant: {
+                // Enable feature B for merchant ABC
+                ABC: {
+                    FEATURE_B: true
+                }
             }
         }
     }
