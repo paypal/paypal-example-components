5130feb4935dd4634cee0ae428700eb82bf194d7
Westin Wrzesinski <westin.wrzesinski@gmail.com>
2022-04-06 11:11:41 -0500

    chore: upgrade flow to 155

--- src/types.js
+++ src/types.js
@@ -4,7 +4,10 @@ export type ExamplePayServerConfigType = {|
     assetsUrl : string
 |};
 
+
 export type ExamplePayGlobalType = {|
     serverConfig : ExamplePayServerConfigType
 |};
 
+
+declare var __example_pay__ : ExamplePayGlobalType;
